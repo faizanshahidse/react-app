99e8756c92d5bec8e7b93907fd5f801c30826388
Faizan Shahid <faizanshahid@Faizans-MacBook-Pro.local>
2025-01-07 11:46:11 +0500

    complete auth functionlity added, signup, context, login and integrated with react hook form

---
+++ src/components/SplashScreen/SplashScreen.jsx
@@ -0,0 +1,13 @@
+import styles from "./SplashScreen.module.scss";
+
+const SplashScreen = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.iconContainer}>
+        <div>...loading</div>
+      </div>
+    </div>
+  );
+};
+
+export default SplashScreen;
